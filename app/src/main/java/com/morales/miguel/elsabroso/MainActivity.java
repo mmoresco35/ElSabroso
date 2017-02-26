b20dccde23dda5f1216f7683c9b7c1f6af8933a5
mmoresco35 <mmoresco35@hotmail.com>
2017-02-26 11:04:10 +0100

    Initial commit version 1.0

---
+++ app/src/main/java/com/morales/miguel/elsabroso/MainActivity.java
@@ -0,0 +1,16 @@
+package com.morales.miguel.elsabroso;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        }
+
+}
